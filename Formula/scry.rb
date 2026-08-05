54d25f57dd1aa36356189bb1750bcd6cd9ad0da2
midagedev <139564968+midagedev@users.noreply.github.com>
2026-08-05 17:42:25 +0900

    scry 0.3.0

--- Formula/scry.rb
+++ Formula/scry.rb
@@ -1,4 +1,4 @@
-# Generated for scry v0.2.1. Future releases update this file from goreleaser
+# Generated for scry v0.3.0. Future releases update this file from goreleaser
 # (.goreleaser.yaml `brews:`); until the tap token exists it is written by hand
 # from the release's own checksums.txt.
 class Scry < Formula
@@ -8,23 +8,23 @@ class Scry < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/midagedev/scry/releases/download/v0.2.1/scry_0.2.1_darwin_arm64.tar.gz"
-      sha256 "06e21574e36663ced29754c1b75bb97aa5c60ea4e0ed5ef5eb102f5580ad541d"
+      url "https://github.com/midagedev/scry/releases/download/v0.3.0/scry_0.3.0_darwin_arm64.tar.gz"
+      sha256 "db46a60151533a554ae1bb25808951ffe8fc48dc795b15b8edbca3f8906aeefd"
     end
     on_intel do
-      url "https://github.com/midagedev/scry/releases/download/v0.2.1/scry_0.2.1_darwin_amd64.tar.gz"
-      sha256 "eb34d84b8c5a51eff04cbf90ef962952c10d15fae17b3bbef88b3c9d9664131a"
+      url "https://github.com/midagedev/scry/releases/download/v0.3.0/scry_0.3.0_darwin_amd64.tar.gz"
+      sha256 "4fc3a25fa224efc32e5c05106bebc2d03908df3511a6fbd4a75903b6b5a2b1a0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/midagedev/scry/releases/download/v0.2.1/scry_0.2.1_linux_arm64.tar.gz"
-      sha256 "39081c5de8d5b10180303997ecdbc241fa523911cd16627fc30859b3f521a05d"
+      url "https://github.com/midagedev/scry/releases/download/v0.3.0/scry_0.3.0_linux_arm64.tar.gz"
+      sha256 "bfb06067f6400705826a3d03baa952eaceb9832fcf12581d5825c955dc015e43"
     end
     on_intel do
-      url "https://github.com/midagedev/scry/releases/download/v0.2.1/scry_0.2.1_linux_amd64.tar.gz"
-      sha256 "9ca129194358f9df7d15d0038bb259e66df707455f8479ce8676b62629867575"
+      url "https://github.com/midagedev/scry/releases/download/v0.3.0/scry_0.3.0_linux_amd64.tar.gz"
+      sha256 "4660751cab3b1d86cab61745c75291c5747dc1ae18b5928121b0b8cc4f4f9e6b"
     end
   end
 
